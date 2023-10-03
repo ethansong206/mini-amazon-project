17388f5d6a6fd596e20e7d30c3dad4beaa7adfee
Miles Eng <miles.eng@duke.edu>
2023-10-03 14:08:52 -0400

    completed add to wishlist functionality

--- app/wishlist.py
+++ app/wishlist.py
@@ -2,7 +2,11 @@ from flask import render_template
 from flask import jsonify
 from flask import redirect, url_for
 from flask_login import current_user
+from humanize import naturaltime
 import datetime
+
+def humanize_time(dt):
+    return naturaltime(datetime.datetime.now() - dt)
 
 from .models.wishlist import WishlistItem
 
@@ -12,10 +16,12 @@ bp = Blueprint('wishlist', __name__)
 @bp.route('/wishlist')
 def wishlist():
     if not current_user.is_authenticated:
-        return jsonfiy({}), 404
+        return jsonify({}), 404
 
     items = WishlistItem.get_all_by_uid(current_user.id)
-    return jsonify([item.__dict__ for item in items])
+    return render_template('wishlist.html',
+                      items=items,
+                      humanize_time=humanize_time)
 
 @bp.route('/wishlist/add/<int:product_id>', methods=['POST'])
 def wishlist_add(product_id):
