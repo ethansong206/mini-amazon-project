ddd3a42bf5b1819f48b6478c2a78d30b55656b91
Ethan Song <es370@duke.edu>
2023-10-03 18:18:40 -0400

    add to wishlist API endpoint implemented

--- app/wishlist.py
+++ app/wishlist.py
@@ -3,11 +3,14 @@ from flask_login import current_user
 import datetime
 
 from .models.wishlist import WishlistItem
+from .models.product import Product
+from .models.purchase import Purchase
 
 from flask import Blueprint
 bp = Blueprint('wishlist', __name__)
 
 from flask import jsonify
+from flask import redirect, url_for
 
 @bp.route('/wishlist')
 def wishlist():
@@ -19,3 +22,9 @@ def wishlist():
         return jsonify({}), 404
     # render the page by adding information to the wishlist.html file
     return jsonify([item.__dict__ for item in items])
+
+@bp.route('/wishlist/add/<int:product_id>', methods=['POST'])
+def wishlist_add(product_id):
+    #add an item into wishlist
+    WishlistItem.wishlist_add(current_user.id, product_id, datetime.datetime.now())
+    return redirect(url_for('wishlist.wishlist'))
