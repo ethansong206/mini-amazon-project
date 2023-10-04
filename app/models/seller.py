5870873d637ec77a8c2798d4d75dd517c1691c9c
Jonny Chang <jonathanchang0805@live.ca>
2023-10-04 18:58:21 -0400

    Add seller model

---
+++ app/models/seller.py
@@ -0,0 +1,18 @@
+from flask import current_app as app
+
+class Seller:
+    def __init__(self, id):
+        self.self = self
+        self.id = id
+
+    @staticmethod
+    def get(id):
+        rows = app.db.execute('''
+SELECT id
+FROM Sellers
+WHERE id = :id
+''',
+                              id=id)
+        return Seller(*(rows[0])) if rows is not None else None
+
+
