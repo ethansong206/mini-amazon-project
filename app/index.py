8d2c86a6df242700d52dd89a846f57b6b066aeba
Jonny Chang <jtc59@duke.edu>
2023-10-05 18:27:19 -0400

    Add button to go to seller view

--- app/index.py
+++ app/index.py
@@ -5,6 +5,7 @@ import datetime
 from .models.product import Product
 from .models.purchase import Purchase
 from .models.user import User
+from .models.seller import Seller
 
 from flask import Blueprint
 bp = Blueprint('index', __name__)
@@ -12,14 +13,18 @@ bp = Blueprint('index', __name__)
 
 @bp.route('/')
 def index():
+    is_seller = False
     # get all available products for sale:
     products = Product.get_all(1)
     # find the products current user has bought:
     if current_user.is_authenticated:
         purchases = User.get_purchase_history(current_user.id)
+        if Seller.get(current_user.id):
+            is_seller = True
     else:
         purchases = None
     # render the page by adding information to the index.html file
     return render_template('index.html',
                            avail_products=products,
-                           purchase_history=purchases)
+                           purchase_history=purchases,
+                           is_seller=is_seller)
